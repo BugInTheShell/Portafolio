fbd3945892eef43b9b5fe65f8f29738393704db6
EstebanGR69 <egalvan@dragontec.net>
2025-06-21 22:24:20 -0600

    add: (Correccion de texto en primer sección)

--- src/components/WebMasterExperience/index.jsx
+++ src/components/WebMasterExperience/index.jsx
@@ -2,16 +2,13 @@ import TitlesH1 from "../TitlesH1"
 
 const WebMasterExperience = () => {
   return (
-    <div className="h-screen">
+    <div className="">
         <TitlesH1 texto="Web Master" style={"silkscreen-regular"} />
         <p className="mt-4 text-3xl text-gray-100/75 vt323-regular">
-            Como webmaster, he desarrollado una sólida experiencia en la creación, gestión y mantenimiento de sitios web utilizando WordPress. A lo largo de mi formación y práctica, he aprendido a configurar y personalizar sitios con una amplia variedad de plugins, optimizando tanto su funcionalidad como su rendimiento.
-
-            Tengo experiencia en la administración de e-commerces en WordPress, gestionando productos, pedidos y configuraciones específicas para tiendas en línea. Además, sé trabajar con plataformas de hosting como SiteGround, donde he aprendido a administrar sitios web, realizar respaldos automáticos y manuales, y asegurar el correcto funcionamiento del entorno web.
-
-            Complementando mis conocimientos en CMS, también domino HTML y CSS, lo que me permite realizar ajustes personalizados y diseñar sitios con una estructura limpia y profesional. Asimismo, me especializo en diseños responsivos, asegurando que cada sitio que desarrollo se vea y funcione correctamente en cualquier dispositivo.
-
-            Mi enfoque es mantener sitios web seguros, actualizados y visualmente atractivos, siempre orientado a una excelente experiencia de usuario.
+            Como webmaster, tengo experiencia en la creación y mantenimiento de sitios web con WordPress, incluyendo la configuración de plugins y la administración de tiendas en línea. 
+            Manejo plataformas como SiteGround para respaldos y gestión de hosting. 
+            Domino HTML y CSS, lo que me permite realizar ajustes personalizados y crear diseños responsivos. 
+            Mi enfoque es mantener sitios seguros, funcionales y optimizados para una buena experiencia de usuario.
         </p>
     </div>
   )
